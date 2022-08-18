b2b37a43292c5b1e5faaf206906daf5df044b212
Yasir Fayrooz <yasir.fayrooz@hotmail.com>
2022-08-18 12:36:35 +1000

    intro text first animation

--- portfolio/components/intro/intro-text.tsx
+++ portfolio/components/intro/intro-text.tsx
@@ -2,10 +2,17 @@ import styles from './intro-text.module.css';
 
 const IntroText = () => {
   return (
-    <div className={styles.text}>
+    <div className={styles.text + ' ' + styles.typing}>
       <p>\[._.]/ Hey there👋 I'm Yasir Fayrooz</p>
       <p>
-        <span>$ </span> A "Full Stack" Engineer
+        <span className="text-green-300">$ </span> A "Full Stack" Engineer
+      </p>
+      <p>
+        <span className="text-green-300">$ </span> Welcome to my world
+      </p>
+      <p>
+        <span className="text-green-300">$ </span> Open my terminal to learn more{' '}
+        <span className="text-sky-300 underline cursor-pointer">here</span>
       </p>
     </div>
   );
